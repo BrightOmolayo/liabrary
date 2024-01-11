066144214e559baa499c94d5926e7eeaadd6f1c0
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-01-11 13:13:12 +0100

    dialog

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+const updateButton = document.getElementById("updateDetails");
+const cancelButton = document.getElementById("cancel");
+const dialog = document.getElementById("favDialog");
+//dialog.returnValue = "favAnimal";
+
+function openCheck(dialog) {
+  if (dialog.open) {
+    console.log("Dialog open");
+  } else {
+    console.log("Dialog closed");
+  }
+}
+
+// Update button opens a modal dialog
+updateButton.addEventListener("click", () => {
+  dialog.showModal();
+  openCheck(dialog);
+});
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener("click", () => {
+  dialog.close("animalNotChosen");
+  openCheck(dialog);
+});
+
 const myLibrary = [];
 
 function Book(name,author,status,year,pages) {
