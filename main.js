5c54acd067659d783ae0f825374710c17d3abfa1
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-01-11 11:32:37 +0100

    loop

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const myLibrary = [];
 
-function Book(name,author,status,year) {
+function Book(name,author,status,year,pages) {
   this.name = name;
-  this.auhtor = auhtor;
-  this.status = status;
+  this.auhtor = author;
   this.year = year;
+  this.pages = pages;
+  this.status = status;
 }
 
 function addBookToLibrary() {
   // do stuff here
+  let input = document.getElementsByClassName("input").value
+  myLibrary.push(input)
+}
+
+
+//loop through my array
+function loop(myLibrary){
+  for (let i = 0; 1 < myLibrary.length; 1++) {
+    return  myLibrary={i};
+    
+  }
 }
